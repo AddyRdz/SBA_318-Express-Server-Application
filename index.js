25f20365059b70ade517972a861ced6afd130c02
Addy Rodriguez <rdz.33.a@gmail.com>
2024-10-14 21:02:40 -0500

    set up basic route

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express()
 
 const PORT = 4000;
 
-
+// routes
+app.get('/', (req,res) => {
+    res.send('Hello')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on: ${PORT}`);
